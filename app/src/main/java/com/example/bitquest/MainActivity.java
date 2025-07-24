9e66c2cf1b9fea73a3c5edb4da0d9a5b075f7904
Giovanni Semioli <g.semioli1@stundenti.unisa.it>
2025-07-24 16:05:00 +0200

    Implemented LoginPage

--- app/src/main/java/com/example/bitquest/MainActivity.java
+++ app/src/main/java/com/example/bitquest/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.bitquest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +15,9 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText email;
+    EditText password;
+    Button registrati;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        email = findViewById(R.id.InputEmail);
+        password = findViewById(R.id.InputPassword);
+        /*controllo presenza dell'email e della password*/
+
+    }
+
+    public void Registrati(){
+
+    }
+
+    public void Accesso(View view) {
+        try {
+            Intent homePage = new Intent(getApplicationContext(), HomePage.class);
+            startActivity(homePage);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Errore: " + e.getMessage(), Toast.LENGTH_LONG).show();
+        }
     }
 }
