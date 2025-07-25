38f4e0347989dd057d37c8413913d69564f9d6e1
Giovanni Semioli <g.semioli1@stundenti.unisa.it>
2025-07-25 10:47:21 +0200

    Implemented HomePage

--- app/src/main/java/com/example/bitquest/HomePage.java
+++ app/src/main/java/com/example/bitquest/HomePage.java
@@ -1,6 +1,11 @@
 package com.example.bitquest;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class HomePage extends AppCompatActivity {
+    private String problem_solving ="Problem Solving";
+    private String concetti_base = "Concetti Base di Informatica";
+    private String concetti_avanzati = "Concetti Avanzati di Informatica";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +28,33 @@ public class HomePage extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        LinearLayout problem = findViewById(R.id.card_problem_solving);
+        TextView textProblem = problem.findViewById(R.id.cardTitle);
+        textProblem.setText(problem_solving);
+        TextView desctiptionProblem = problem.findViewById(R.id.cardDescription);
+        desctiptionProblem.setText("Questa è la descrizione della categoria Problem Solving");
 
+        LinearLayout concettiB = findViewById(R.id.card_base_info);
+        TextView textConcettiB = concettiB.findViewById(R.id.cardTitle);
+        textConcettiB.setText(concetti_base);
+        TextView desctiptionConcettiB = concettiB.findViewById(R.id.cardDescription);
+        desctiptionConcettiB.setText("Questa è la descrizione della categoria Concetti Base di Informatica");
+
+        LinearLayout concettiA = findViewById(R.id.card_advanced_info);
+        TextView textConcettiA = concettiA.findViewById(R.id.cardTitle);
+        textConcettiA.setText(concetti_avanzati);
+        TextView desctiptionConcettiA = concettiA.findViewById(R.id.cardDescription);
+        desctiptionConcettiA.setText("Questa è la descrizione della categoria Concetti Avanzati di Informatica");
+
+
+
+    }
+    public void goToPuzzle(View view){
+        LinearLayout category = (LinearLayout) view.getParent();
+        TextView titleCategory = category.findViewById(R.id.cardTitle);
+        String title = "" + titleCategory.getText();
+        Intent puzzlePage = new Intent(getApplicationContext(), PuzzlePage.class);
+        puzzlePage.putExtra("CATEGORY", title);
+        startActivity(puzzlePage);
     }
 }
---
+++ app/src/main/java/com/example/bitquest/PuzzlePage.java
@@ -0,0 +1,24 @@
+package com.example.bitquest;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PuzzlePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.puzzle_page);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.puzzle), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
